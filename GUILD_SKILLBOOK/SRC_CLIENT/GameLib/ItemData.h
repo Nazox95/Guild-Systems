efefacb546ada8d1e2d141b4268011f1db8abc93
Nazox <dani6359@hotmail.es>
2025-01-28 23:14:03 +0100

    Create ItemData.h

---
+++ GUILD_SKILLBOOK/SRC_CLIENT/GameLib/ItemData.h
@@ -0,0 +1,10 @@
+//Add in enum EItemType
+		enum EItemType
+		{
+			ITEM_TYPE_NONE,
+			[...]
+#ifdef ENABLE_SKILL_FORGOT_BOOK
+			ITEM_SKILLFORGETGUILD, //37 Libro de olvido gremial
+#endif
+			ITEM_TYPE_MAX_NUM,
+		};
