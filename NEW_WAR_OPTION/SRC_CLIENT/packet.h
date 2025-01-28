5e3be18895cfb6cae06948b886a89071be913b44
Nazox <dani6359@hotmail.es>
2025-01-28 22:46:32 +0100

    Create packet.h

---
+++ NEW_WAR_OPTION/SRC_CLIENT/packet.h
@@ -0,0 +1,13 @@
+//Open Packet.h find and replace 
+typedef struct packet_guild_war
+{
+	DWORD	   dwGuildSelf;
+	DWORD	   dwGuildOpp;
+	BYTE		bType;
+	BYTE		bWarState;
+#ifdef ENABLE_NEW_WAR_OPTIONS
+	BYTE		bRound;
+	BYTE		bPoints;
+	DWORD		dwTime;
+#endif
+} TPacketGCGuildWar;
