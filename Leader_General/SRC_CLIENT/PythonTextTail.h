0ee36688ab1490210fd25184f95dea041a87657e
Nazox <dani6359@hotmail.es>
2025-01-29 20:48:56 +0100

    Delete Leader_General directory

--- Leader_General/SRC_CLIENT/PythonTextTail.h
+++
@@ -1,6 +0,0 @@
-//Open PythonTextTail.h and replace:
-#ifdef ENABLE_SHOW_LIDER_AND_GENERAL_GUILD
-		void RegisterCharacterTextTail(DWORD dwGuildID, BYTE m_dwNewIsGuildName, DWORD dwVirtualID, const D3DXCOLOR& c_rColor, float fAddHeight = 10.0f);
-#else
-		void RegisterCharacterTextTail(DWORD dwGuildID, DWORD dwVirtualID, const D3DXCOLOR& c_rColor, float fAddHeight = 10.0f);
-#endif
