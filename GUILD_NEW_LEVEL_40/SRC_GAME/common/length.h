f3e6bc5071a2b60922a3d829bf0c8a85c6030b17
Nazox <dani6359@hotmail.es>
2025-01-29 19:52:54 +0100

    Update length.h

--- GUILD_NEW_LEVEL_40/SRC_GAME/common/length.h
+++ GUILD_NEW_LEVEL_40/SRC_GAME/common/length.h
@@ -5,8 +5,9 @@
 GUILD_MAX_LEVEL			= 20,
 
 //Replace with this:
-#if defined(ENABLE_EXTENDED_GUILD_LEVEL)
-	GUILD_MAX_LEVEL			= 40, //Lv 40 for guilds
+#if defined(ENABLE_GUILDRENEWAL_SYSTEM) && defined(ENABLE_EXTENDED_GUILD_LEVEL)
+	GUILD_MAX_LEVEL			= 40,
+	MAX_GUILD_DONATION_PER_DAY = 3,
 #else
-	GUILD_MAX_LEVEL			= 20, //Original Level
+	GUILD_MAX_LEVEL			= 20,
 #endif
