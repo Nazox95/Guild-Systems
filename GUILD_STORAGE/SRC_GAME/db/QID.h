350d471c826e7ebb8ee5470e424113b420e64d52
Nazox <dani6359@hotmail.es>
2025-01-29 18:59:04 +0100

    Create QID.h

---
+++ GUILD_STORAGE/SRC_GAME/db/QID.h
@@ -0,0 +1,12 @@
+//Open QID.h find 
+	// MYSHOP_PRICE_LIST
+    QID_ITEMPRICE_SAVE,			///< 21, ¾ÆÀÌÅÛ °¡°ÝÁ¤º¸ ÀúÀå Äõ¸®
+    QID_ITEMPRICE_DESTROY,		///< 22, ¾ÆÀÌÅÛ °¡°ÝÁ¤º¸ »èÁ¦ Äõ¸®
+    QID_ITEMPRICE_LOAD_FOR_UPDATE,	///< 23, °¡°ÝÁ¤º¸ ¾÷µ¥ÀÌÆ®¸¦ À§ÇÑ ¾ÆÀÌÅÛ °¡°ÝÁ¤º¸ ·Îµå Äõ¸®
+    QID_ITEMPRICE_LOAD,			///< 24, ¾ÆÀÌÅÛ °¡°ÝÁ¤º¸ ·Îµå Äõ¸®
+	// END_OF_MYSHOP_PRICE_LIST
+//add
+#ifdef ENABLE_GUILDSTORAGE_SYSTEM
+    QID_GUILDSTORAGE_CHANGE_SIZE,
+#endif
+};
