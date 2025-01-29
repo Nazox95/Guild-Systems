bfc2bfb56523881704c3d1c7cc91cee04b28ac76
Nazox <dani6359@hotmail.es>
2025-01-29 20:48:04 +0100

    Create guild.cpp

---
+++ ENABLE_SHOW_LIDER_AND_GENERAL_GUILD/SRC_GAME/guild.cpp
@@ -0,0 +1,31 @@
+//Open guild.cpp and find:
+DWORD CGuild::GetMemberPID(const std::string& strName)
+{
+	[...]
+}
+//Add after this:
+#ifdef ENABLE_SHOW_LIDER_AND_GENERAL_GUILD
+BYTE CGuild::NewIsGuildGeneral(DWORD pid)
+{
+	for ( TGuildMemberContainer::iterator iter = m_member.begin(); iter != m_member.end(); iter++ )
+	{
+		if ( iter->first == pid )
+			return iter->second.is_general;
+	}
+	return 0;
+}
+
+bool CGuild::IsCoLeader(DWORD pid)
+{
+	for ( TGuildMemberContainer::iterator iter = m_member.begin(); iter != m_member.end(); iter++ )
+	{
+		if ( iter->first == pid )
+			if (iter->second.grade == GUILD_COLEADER_GRADE)
+				return true;
+			else
+				return false;
+	}
+
+	return false;
+}
+#endif
