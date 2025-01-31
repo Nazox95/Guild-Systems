de61807cedad0fd946693fabe47f3db0450c597f
Nazox <dani6359@hotmail.es>
2025-01-31 18:08:07 +0100

    Delete ##UPDATES##/SRC_GAME directory

--- ##UPDATES##/SRC_GAME/UPDATE#1/packet.h
+++
@@ -1,9 +0,0 @@
-//Open packet.h, find and replace [UPDATE 30/01/2025]
-
-#ifdef ENABLE_GUILDSTORAGE_SYSTEM
-typedef struct packet_guildstorage_open
-{
-	BYTE bHeader;
-	BYTE bSize;
-} TPacketGCGuildstorageOpen;
-#endif
