695dadea3c3e46d69885f8e043376c56d23545df
Nazox <dani6359@hotmail.es>
2025-01-31 18:20:40 +0100

    Create length.h

---
+++ ##UPDATE##/[UPDATE 31-01-2025]/SRC CLIENT/SRC GAME/common/length.h
@@ -0,0 +1,20 @@
+//Open length.h and replace
+#if defined(ENABLE_GUILDRENEWAL_SYSTEM) || defined(ENABLE_GUILDBANK_LOG)
+enum EGuildLogInOut
+{
+	GUILD_GOLD_TYPE_IN = 0,						// Depositar
+	GUILD_GOLD_TYPE_OUT = 1,					// Retirar
+	GUILD_GOLD_TYPE_OBJECT_CREATE = 5,			// Construcción
+	GUILD_GOLD_TYPE_DRAGON_SP_RECOVER = 6,		// SP
+	GUILD_GOLD_TYPE_BUY_GUILD_LAND = 7,			// Tierras
+	GUILD_GOLD_TYPE_CHANGE_SMELTER = 8,			// Hornos
+	GUILD_GOLD_TYPE_UPGRADE_ALTEROFPOWER = 9,	// Altar
+	GUILD_GOLD_TYPE_UPGRADE_GUILDBANK = 10,		// Aknacén
+};
+//Fix
+enum EGuildLogItemInOut
+{
+	GUILD_ITEM_TYPE_IN = 0,
+	GUILD_ITEM_TYPE_OUT = 1
+};
+#endif
