5c7b66fb1dfdaf7b48d0687ae313babe9e846cb6
Nazox <dani6359@hotmail.es>
2025-01-31 17:53:07 +0100

    Create PythonPlayerModule.cpp

---
+++ ##UPDATES##/SRC_CLIENT/UPDATE#2/PythonPlayerModule.cpp
@@ -0,0 +1,17 @@
+//add
+#ifdef ENABLE_SECOND_GUILDRENEWAL_SYSTEM
+PyObject* playerSetParalysis(PyObject* poSelf, PyObject* poArgs)
+{
+	BOOL bState = 0;
+	if (!PyTuple_GetInteger(poArgs, 0, &bState))
+		return Py_BadArgument();
+
+	CPythonPlayer::Instance().SetParalysis(bState);
+	return Py_BuildNone();
+}
+#endif
+
+//Now add in void init
+#ifdef ENABLE_SECOND_GUILDRENEWAL_SYSTEM
+		{ "SetParalysis",		playerSetParalysis,		METH_VARARGS },
+#endif
